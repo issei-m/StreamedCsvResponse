9b9365eefcc92c7688f3028f32b1fcecf2fe6d0b
Issei.M <issei.m7@gmail.com>
2016-04-14 23:09:04 +0900

    added failure test case to cover PHP's bug

--- tests/StreamedCsvResponseTest.php
+++ tests/StreamedCsvResponseTest.php
@@ -4,15 +4,59 @@ namespace Issei\Tests;
 
 use Issei\StreamedCsvResponse;
 
+class Stringable
+{
+    private $string;
+    private $memoryOverflowMaker;
+
+    public function __construct($string)
+    {
+        $this->string = $string;
+
+        // To test whether no error occurs when recursion is passed to response.
+        // See: https://bugs.php.net/bug.php?id=66964
+        $mof = array();
+        $mof[] = &$mof;
+        $this->memoryOverflowMaker = $mof;
+    }
+
+    public function __toString()
+    {
+        return $this->string;
+    }
+}
+
 class StreamedCsvResponseTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGeneral()
+    public function rowsProvider()
     {
-        $rows = array(
-            array('名前', 'メアド', '性別',),
-            array('田中 太郎', 'taro@example.com', '男性',),
+        return array(
+            'array' => array(
+                array(
+                    array('名前', 'メアド', '性別'),
+                    array('田中 太郎', 'taro@example.com', '男性'),
+                ),
+            ),
+            'Traversable' => array(
+                new \ArrayIterator(array(
+                    array('名前', 'メアド', '性別'),
+                    array('田中 太郎', 'taro@example.com', '男性'),
+                )),
+            ),
+            'containing Stringable' => array(
+                array(
+                    array(new Stringable('名前'), new Stringable('メアド'), new Stringable('性別')),
+                    array(new Stringable('田中 太郎'), new Stringable('taro@example.com'), new Stringable('男性')),
+                ),
+            ),
         );
+    }
 
+    /**
+     * @dataProvider rowsProvider
+     */
+    public function testGeneral($rows)
+    {
         $this->expectOutputString('"名前","メアド","性別"' . "\r\n" . '"田中 太郎","taro@example.com","男性"' . "\r\n");
 
         $response = new StreamedCsvResponse($rows, 'test.csv');
@@ -22,14 +66,19 @@ class StreamedCsvResponseTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('attachment; filename=test.csv', $response->headers->get('Content-disposition'));
     }
 
-    public function testWithShiftJIS()
+    /**
+     * @dataProvider rowsProvider
+     */
+    public function testWithShiftJIS($rows)
     {
-        $rows = array(
-            array('名前',),
-            array('田中 太郎',),
+        $this->expectOutputString(
+            '"' . mb_convert_encoding('名前', 'Shift-JIS', 'UTF-8') . '",' .
+            '"' . mb_convert_encoding('メアド', 'Shift-JIS', 'UTF-8') . '",' .
+            '"' . mb_convert_encoding('性別', 'Shift-JIS', 'UTF-8') . '"' . "\r\n" .
+            '"' . mb_convert_encoding('田中 太郎', 'Shift-JIS', 'UTF-8') . '",' .
+            '"taro@example.com",' .
+            '"' . mb_convert_encoding('男性', 'Shift-JIS', 'UTF-8') . '"' . "\r\n"
         );
-
-        $this->expectOutputString('"' . mb_convert_encoding('名前', 'Shift-JIS', 'UTF-8') . '"' . "\r\n" . '"' . mb_convert_encoding('田中 太郎', 'Shift-JIS', 'UTF-8') . '"' . "\r\n");
 
         $response = new StreamedCsvResponse($rows, 'foobar.csv');
         $response->setCharset('Shift-JIS');
